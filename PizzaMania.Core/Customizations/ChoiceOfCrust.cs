63fcad1f1f8502d359bd498a5486c221cc2c92d1
tavisca-vraut <vraut@tavisca.com>
2019-12-07 18:00:19 +0530

    add the choiceOfCrust customization

---
+++ PizzaMania.Core/Customizations/ChoiceOfCrust.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace PizzaMania.Core.Customizations
+{
+    public enum Crust
+    {
+        Default, PanTossed, CheeseBlast, ThisCrustCheeseBlast, ChickenCheeseBlast
+    }
+    public class ChoiceOfCrust
+    {
+        public Crust Value { get; }
+
+        public ChoiceOfCrust(Crust crust = Crust.Default)
+        {
+            Value = crust;
+        }
+
+        public float GetPrice()
+        {
+            return CrustPrices.GetPriceFor(Value);
+        }
+    }
+
+    public static class CrustPrices
+    {
+        static Dictionary<Crust, float> PriceMap = new Dictionary<Crust, float>()
+        {
+            { Crust.CheeseBlast, 125 },
+            { Crust.ChickenCheeseBlast, 150 },
+            { Crust.Default, 100 }
+        };
+
+        public static float GetPriceFor(Crust crust)
+        {
+            return PriceMap.GetValueOrDefault(crust, 200);
+        }
+    }
+}
---
+++ PizzaMania.Tests/Customizations.Tests/ChoiceOfCrustFixtures.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+using FluentAssertions;
+
+using PizzaMania.Core.Customizations;
+
+namespace PizzaMania.Tests.Customizations.Tests
+{
+    public class ChoiceOfCrustFixtures
+    {
+        ChoiceOfCrust ChoiceOfCrust { get; set; }
+        public ChoiceOfCrustFixtures()
+        {
+            ChoiceOfCrust = new ChoiceOfCrust();
+        }
+
+        [Fact]
+        public void Test_for_initialization_of_choice_of_crust()
+        {
+            this.ChoiceOfCrust.Value.Should().Be(Crust.Default);
+        }
+
+        [Fact]
+        public void Test_for_getting_price_of_choice_of_crust()
+        {
+            var actualPrice = ChoiceOfCrust.GetPrice();
+            var expectedPrice = CrustPrices.GetPriceFor(ChoiceOfCrust.Value);
+
+            actualPrice.Should().Be(expectedPrice);
+        }
+    }
+}
