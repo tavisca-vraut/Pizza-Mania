7d3fd24d96d540f14ad7c094c50342010d8679c6
tavisca-vraut <vraut@tavisca.com>
2019-12-07 19:08:18 +0530

    add the choiceOfToppingsFunctionality

---
+++ PizzaMania.Core/Customizations/ChoiceOfToppings.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+
+namespace PizzaMania.Core.Customizations
+{
+    public enum VegTopping
+    { 
+        PaneerCubes, Mushrooms, BlackOlives, SpicyJalepenos
+    }
+
+    public enum NonVegTopping
+    {
+        BarbequeChicken, ChickenSausage, RoastChicken, SpicyChicken, HamSlices, ShreddedBeef
+    }
+
+    public static class ToppingsPrices
+    {
+        static Dictionary<VegTopping, float> VegToppingsPriceMap = new Dictionary<VegTopping, float>()
+        {
+            { VegTopping.PaneerCubes, 200 },
+            { VegTopping.Mushrooms, 125 }
+        };
+
+        static Dictionary<NonVegTopping, float> NonVegToppingsPriceMap = new Dictionary<NonVegTopping, float>()
+        {
+            { NonVegTopping.BarbequeChicken, 200 },
+            { NonVegTopping.ShreddedBeef, 400 }
+        };
+
+        public static float GetPriceFor(VegTopping vegTopping)
+        {
+            return VegToppingsPriceMap.GetValueOrDefault(vegTopping, 150);
+        }
+
+        public static float GetPriceFor(NonVegTopping nonVegTopping)
+        {
+            return NonVegToppingsPriceMap.GetValueOrDefault(nonVegTopping, 200);
+        }
+    }
+
+    public class ChoiceOfToppings
+    {
+        public List<VegTopping> VegToppings { get; private set; }
+        public List<NonVegTopping> NonVegToppings { get; private set; }
+
+        public ChoiceOfToppings()
+        {
+            VegToppings = new List<VegTopping>();
+            NonVegToppings = new List<NonVegTopping>();
+        }
+
+        public void AddToVeg(VegTopping vegTopping)
+        {
+            if (VegToppings.Contains(vegTopping) == false)
+            {
+                VegToppings.Add(vegTopping);
+            }
+        }
+
+        public void AddToNonVeg(NonVegTopping nonVegTopping)
+        {
+            if (NonVegToppings.Contains(nonVegTopping) == false)
+            {
+                NonVegToppings.Add(nonVegTopping);
+            }
+        }
+
+        public float GetToppingsPrice()
+        {
+            var price = 0f;
+
+            foreach (var vegTopping in VegToppings)
+            {
+                price += ToppingsPrices.GetPriceFor(vegTopping);
+            }
+
+            foreach (var nonVegTopping in NonVegToppings)
+            {
+                price += ToppingsPrices.GetPriceFor(nonVegTopping);
+            }
+
+            return price;
+        }
+    }
+}
---
+++ PizzaMania.Tests/Customizations.Tests/ChoiceOfToppingsFixtures.cs
@@ -0,0 +1,77 @@
+﻿using Xunit;
+using FluentAssertions;
+
+using PizzaMania.Core.Customizations;
+
+namespace PizzaMania.Tests.Customizations.Tests
+{
+    public class ChoiceOfToppingsFixtures
+    {
+        public ChoiceOfToppings ChoiceOfToppings{ get; set; }
+
+        public ChoiceOfToppingsFixtures()
+        {
+            ChoiceOfToppings = new ChoiceOfToppings();
+        }
+
+        [Fact]
+        public void Test_for_initialization_of_toppings()
+        {
+            ChoiceOfToppings.NonVegToppings.Count.Should().Be(0);
+            ChoiceOfToppings.VegToppings.Count.Should().Be(0);
+        }
+
+        [Fact]
+        public void Test_for_additions_to_veg_toppings()
+        {
+            ChoiceOfToppings.AddToVeg(VegTopping.PaneerCubes);
+
+            ChoiceOfToppings.VegToppings.Count.Should().Be(1);
+            ChoiceOfToppings.VegToppings.Should().Contain(VegTopping.PaneerCubes);
+        }
+
+        [Fact]
+        public void Test_for_additions_to_non_veg_toppings()
+        {
+            ChoiceOfToppings.AddToNonVeg(NonVegTopping.ShreddedBeef);
+
+            ChoiceOfToppings.NonVegToppings.Count.Should().Be(1);
+            ChoiceOfToppings.NonVegToppings.Should().Contain(NonVegTopping.ShreddedBeef);
+        }
+
+        [Fact]
+        public void Test_for_avoiding_additions_of_duplicate_toppings()
+        {
+            ChoiceOfToppings.AddToVeg(VegTopping.PaneerCubes);
+            ChoiceOfToppings.AddToNonVeg(NonVegTopping.ShreddedBeef);
+
+            ChoiceOfToppings.AddToVeg(VegTopping.PaneerCubes);
+            ChoiceOfToppings.AddToNonVeg(NonVegTopping.ShreddedBeef);
+
+            ChoiceOfToppings.VegToppings.Count.Should().Be(1);
+            ChoiceOfToppings.VegToppings.Should().Contain(VegTopping.PaneerCubes);
+
+            ChoiceOfToppings.NonVegToppings.Count.Should().Be(1);
+            ChoiceOfToppings.NonVegToppings.Should().Contain(NonVegTopping.ShreddedBeef);
+        }
+
+        [Fact]
+        public void Test_for_toppings_prices()
+        {
+            ChoiceOfToppings.AddToVeg(VegTopping.PaneerCubes);  // 200
+            ChoiceOfToppings.AddToVeg(VegTopping.SpicyJalepenos);  // 150
+            ChoiceOfToppings.AddToVeg(VegTopping.BlackOlives);  // 150
+            // total = 500
+
+            ChoiceOfToppings.AddToNonVeg(NonVegTopping.HamSlices);  // 200
+            ChoiceOfToppings.AddToNonVeg(NonVegTopping.ShreddedBeef);  // 400
+            // total = 600
+
+
+            var actualPrice = ChoiceOfToppings.GetToppingsPrice();
+            var expectedPrice = 1100f;
+
+            actualPrice.Should().Be(expectedPrice);
+        }
+    }
+}
